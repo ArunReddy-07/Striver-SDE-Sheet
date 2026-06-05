833dd31100fb6d874a3eeb82cf9b90a336895786
ArunReddy-07 <arunreddybhumireddy@gmail.com>
2026-06-05 23:10:03 +0530

    Add setZeroes method to modify matrix in place
    Implemented a method to set entire rows and columns to zero in a matrix if any element is zero.

--- Arrays/SetMatrixZeroes.java
+++ Arrays/SetMatrixZeroes.java
@@ -1,1 +1,27 @@
-
+/*
+create an boolean arrays row and col and when a row contains a element zero set row element = 0 and similary col
+after this the or operation between two boolean arrays provide the entire row and coloumn to zero.
+*/
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        boolean rows[] =  new boolean[n];
+        boolean col[] = new boolean[m];
+        for(int i=0;i<n;i++){
+         for(int j=0;j<m;j++){
+            if(matrix[i][j]==0){
+                rows[i] =  true;
+                col[j] = true;
+            }
+         }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(rows[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
